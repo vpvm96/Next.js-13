eacaee3d47f2796dbd98a17aa514d74b920ae9a9
vpvm96 <vpvm96@gmail.com>
2023-04-05 22:49:20 +0900

    feat: ChatInput UI 구현

---
+++ app/ChatInput.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { FormEvent, useState } from "react"
+
+export default function ChatInput() {
+  const [input, setInput] = useState("")
+
+  const handleMessageSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!input) return
+
+    const messageToSend = input
+
+    setInput("")
+  }
+
+  return (
+    <form
+      onSubmit={handleMessageSubmit}
+      className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100"
+    >
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter Message Here..."
+        className="
+        flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 
+        focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed
+        "
+      />
+      <button
+        type="submit"
+        disabled={!input}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Send
+      </button>
+    </form>
+  )
+}
