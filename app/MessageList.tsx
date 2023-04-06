b5a540cc430f12e8857616811d6a81ef956d7d42
vpvm96 <vpvm96@gmail.com>
2023-04-06 20:01:11 +0900

    feat: MessageList 테스트 작성

---
+++ app/MessageList.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function MessageList() {
+  return (
+    <div>
+      <p>Message</p>
+      <p>Message</p>
+      <p>Message</p>
+      <p>Message</p>
+      <p>Message</p>
+      <p>Message</p>
+      <p>Message</p>
+      <p>Message</p>
+    </div>
+  )
+}
