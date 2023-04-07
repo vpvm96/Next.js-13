fb83a026a7632d532b783b06f50a164c70240aa2
vpvm96 <vpvm96@gmail.com>
2023-04-07 18:23:32 +0900

    feat: Message Component 적용 및 리스트와 컴포넌트 스타일 추가

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -1,16 +1,22 @@
-import React from "react"
+"use client"
+
+import useSWR from "swr"
+import fetcher from "@/utils/fetchMessages"
+import { Message } from "@/type"
+import MessageComponent from "./MessageComponent"
 
 export default function MessageList() {
+  const {
+    data: messages,
+    error,
+    mutate,
+  } = useSWR<Message[]>("/api/getMessages", fetcher)
+
   return (
-    <div>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
+    <div className="space-y-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
+      {messages?.map((message) => (
+        <MessageComponent key={message.id} message={message} />
+      ))}
     </div>
   )
 }
