0b2eba4787c7b2139af499e15c4e1e1b36d668b8
vpvm96 <vpvm96@gmail.com>
2023-04-09 02:19:22 +0900

    feat: Image nextconfig 경로 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["links.papareact.com", "platform-lookaside.fbsbx.com"],
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "www.google.com",
+    ],
   },
   experimental: {
     appDir: true,
